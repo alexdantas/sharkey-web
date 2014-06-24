b5aea715f879306ef2f9cbd5871b8c292683be90
Alexandre Dantas <eu@alexdantas.net>
2014-06-24 00:40:55 -0300

    Created base Link class - started using DataMapper for SQLite database

--- main.rb
+++ main.rb
@@ -1,7 +1,30 @@
 require 'sinatra'
 require 'slim'
+require 'data_mapper'
 
-get '/' do
-  slim :index
+# Represents a single HyperLink specified by the user.
+#
+class Link
+  # This tells that it is a thing that will get stored
+  # on the database
+  # Below are all the database elements
+  include DataMapper::Resource
+
+  property :id,          Serial                    # Auto-incrementing key
+  property :title,       String, :required => true # User-specified title
+  property :added_at,    DateTime                  # When this link was added
+end
+
+# This method must be called after ALL models
+# have been created and BEFORE the app starts
+DataMapper.finalize
+
+begin
+  # Starting out the SQLite Database
+  DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
+
+  get '/' do
+    slim :index
+  end
 end
 
